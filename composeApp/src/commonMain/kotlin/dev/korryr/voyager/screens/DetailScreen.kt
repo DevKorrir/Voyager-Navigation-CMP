6e3c12bd68778cc9885572f1267d756638689ccc
Devkorrir <kipyegonaldo@proton.me>
2026-01-29 17:57:19 +0300

    tweaks on ui and git ignore

--- composeApp/src/commonMain/kotlin/dev/korryr/voyager/screens/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/korryr/voyager/screens/DetailScreen.kt
@@ -2,12 +2,15 @@ package dev.korryr.voyager.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
@@ -24,7 +27,9 @@ data class DetailScreen(
             verticalArrangement = Arrangement.Center
         ) {
             Text("Detail Screen")
+            Spacer(Modifier.height(24.dp))
             Text(textArg)
+            Spacer(Modifier.height(24.dp))
             Button(
                 onClick = {
                     navigator.push(HomeScreen3())
--- composeApp/src/commonMain/kotlin/dev/korryr/voyager/screens/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/korryr/voyager/screens/HomeScreen.kt
@@ -2,12 +2,15 @@ package dev.korryr.voyager.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
@@ -26,6 +29,7 @@ class HomeScreen: Screen {
             verticalArrangement = Arrangement.Center
         ){
             Text("Home Screen")
+            Spacer(Modifier.height(24.dp))
             Button(
                 onClick = {
                     navigator.push(DetailScreen(
